3a36f323b9e9aa776e8dbf0d681cfce3351c977c
Antharuu <antho.eclips@gmail.com>
2021-01-07 14:05:33 +0100

    0.8 Stylus reworked

---
+++ js/bubblegum.js
@@ -0,0 +1,32 @@
+/**
+ * Bubblegum: elegant syntax framework
+ *
+ * @license MIT <https://opensource.org/licenses/MIT>
+ * @author Antharuu <https://github.com/antharuu>
+ * @namespace
+ * @public
+ */
+const bubblegum_prefix = '_';
+
+let codes = document.querySelectorAll('[' + bubblegum_prefix + 'code]');
+
+codes.forEach(function (c) {
+    {
+        let lang = c.attributes[bubblegum_prefix + 'code'].value;
+        c.classList.add('language-' + lang)
+        if (c.localName === 'code') {
+            bubble_set_code(c)
+        } else if (c.localName === 'pre') {
+            if (c.childNodes[0].localName === 'code') {
+                bubble_set_code(c.childNodes[0])
+            }
+        }
+    }
+})
+
+function bubble_set_code(c) {
+    c.innerHTML = c.innerHTML.trim()
+    if (c.parentElement.localName !== 'pre') {
+        c.outerHTML = '<pre>' + c.outerHTML + '</pre>'
+    }
+}
